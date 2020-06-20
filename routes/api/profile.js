bed4587a01f89b9961c5e15b78b54b1773683253
Saurabh Dhamnaskar <309saurabhd@gmail.com>
2020-06-20 16:52:44 -0400

    Implement delete user and profile API

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -126,4 +126,22 @@ router.get('/user/:user_id', async(req, res) => {
   }
 });
 
+// @route   DELETE api/profile/
+// @desc    Delete profile, user and posts
+// @access  Private
+router.delete('/', auth, async(req, res) => {
+  try {
+    // @todo - remove users posts
+    // Remove profile
+    await Profile.findOneAndRemove({user: req.user.id});
+    
+    //Remove user
+    await User.findOneAndRemove({_id: req.user.id});
+    res.json({msg: "User removed successfully!"});
+  } catch (error) {
+    console.error(error)
+    res.status(500).json('Server error')
+  }
+});
+
 module.exports = router;
